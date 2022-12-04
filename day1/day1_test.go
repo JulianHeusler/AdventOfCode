fbc8143c9ff9be33c0fa080ba819a383d86f51be
Julian Heusler <udxqd@kit.edu>
2022-12-04 14:42:04 +0100

    Add test with real data for day1

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -2,6 +2,7 @@ package day1_test
 
 import (
 	"adventofcode/day1"
+	"adventofcode/reader"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -28,4 +29,10 @@ func TestSolveWithTestData(t *testing.T) {
 	resultPart1, resultPart2 := day1.Solve(testInput)
 	assert.Equal(t, 24000, resultPart1)
 	assert.Equal(t, 45000, resultPart2)
+}
+
+func TestSolveRealInput(t *testing.T){
+	resultPart1, resultPart2 := day1.Solve(reader.ReadInput(1, false))
+	assert.Equal(t, 69836, resultPart1)
+	assert.Equal(t, 207968, resultPart2)
 }
