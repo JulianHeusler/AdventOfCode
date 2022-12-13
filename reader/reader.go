8a5647e04b509048b8e39aa6cf3290ba30b963c7
Julian Heusler <julian.heusler@cas.de>
2022-12-13 11:15:43 +0100

    Reader can also read example.txt

--- reader/reader.go
+++ reader/reader.go
@@ -6,14 +6,23 @@ import (
 	"strings"
 )
 
-func ReadInput(day int, rootDir bool) (lines []string) {
-	var filepath string
-	if rootDir {
-		filepath = fmt.Sprintf("day%d/input.txt", day)
-	} else {
-		filepath = fmt.Sprintf("../day%d/input.txt", day)
-	}
+func ReadExampleInput(day int) (lines []string) {
+	return readFile(getFilePath(day, false, "example"))
+}
 
+func ReadInput(day int, isRootDir bool) (lines []string) {
+	return readFile(getFilePath(day, isRootDir, "input"))
+}
+
+func getFilePath(day int, isRootDir bool, filename string) string {
+	if isRootDir {
+		return fmt.Sprintf("day%d/%s.txt", day, filename)
+	} else {
+		return fmt.Sprintf("../day%d/%s.txt", day, filename)
+	}
+}
+
+func readFile(filepath string) (lines []string) {
 	input, err := os.ReadFile(filepath)
 	if err != nil {
 		fmt.Println("Error: ", err.Error())
