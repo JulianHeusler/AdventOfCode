aa0f20f9c070e7ac6b33046201af1e07079a28b7
Julian Heusler <udxqd@kit.edu>
2022-12-14 23:16:44 +0100

    refactor day14

--- day14/day14.go
+++ day14/day14.go
@@ -16,93 +16,88 @@ type Position struct {
 var minX = math.MaxInt
 var maxX = 0
 var maxY = 0
-var xOffset = -1
-var yOffset = -2
+var xOffset = 3
 
-var sandPos = Position{500, 0}
-
+var sandStartingPosition = Position{500, 0}
 var cave [][]rune
 
 func Solve(lines []string) (part1, part2 int) {
-	return solvePart1(lines), solvePart2(lines)
+	pathList := parsePathLines(lines)
+	setMaxBounds(pathList)
+	return solvePart1(pathList), solvePart2(pathList)
 }
 
-func solvePart1(lines []string) int {
-	pathList := parse(lines)
-	setMaxBounds(pathList)
+func solvePart1(pathList [][]Position) int {
 	initCave(pathList, false)
+	setRuneAt('+', sandStartingPosition)
 
-	setRuneAt('+', sandPos)
-
-	//printCave()
-
-	return simulateSandrush(sandPos, false)
+	sandBlockCount := simulateSandblocks(false)
+	printCave()
+	return sandBlockCount
 }
 
-func solvePart2(lines []string) int {
-	pathList := parse(lines)
-	setMaxBounds(pathList)
+func solvePart2(pathList [][]Position) int {
 	initCave(pathList, true)
+	setRuneAt('+', sandStartingPosition)
 
-	sandPos := Position{500, 0}
-	//setRuneAt('+', sandPos)
-
-	// printCave()
-
-	return simulateSandrush(sandPos, true)
+	sandBlockCount := simulateSandblocks(true)
+	printCave()
+	return sandBlockCount
 }
 
 func printCave() {
 	for _, caveLine := range cave {
 		var printLine string
-		for _, caveRune := range caveLine {
-			printLine += string(caveRune)
+		for _, caveBlock := range caveLine {
+			printLine += string(caveBlock)
 		}
 		fmt.Println(printLine)
 	}
 }
 
-func simulateSandrush(startPos Position, hasFloor bool) int {
-	sandBlocks := 0
+func simulateSandblocks(hasFloor bool) (sandBlocks int) {
 	for {
 		fmt.Printf("----------iteration:%0d----------\n", sandBlocks)
-		err, pos := simSandBlock(startPos, hasFloor)
+		sandPosition, err := simSandBlock(sandStartingPosition, hasFloor)
 		if err != nil {
 			fmt.Println(err)
 			return sandBlocks
 		}
-		if pos.x == startPos.x && pos.y == startPos.y {
-			return sandBlocks + 1
-		}
-		// printCave()
+
+		setRuneAt('o', sandPosition)
 		sandBlocks++
+
+		if sandPosition == sandStartingPosition {
+			return sandBlocks
+		}
+
+		// printCave()
 	}
 }
 
-func simSandBlock(pos Position, hasFloor bool) (error, Position) {
+func simSandBlock(position Position, hasFloor bool) (Position, error) {
 	for {
-		if !hasFloor && pos.y > maxY {
-			return errors.New("endless"), Position{}
+		if !hasFloor && position.y > maxY {
+			return Position{}, errors.New("reached abyss")
 		}
-
-		if isAir(pos) {
-			pos.y++
+		if isAir(position) {
+			position.y++
 			continue
 		}
-		diagonalDownLeft := Position{pos.x - 1, pos.y}
-		diagonalDownRight := Position{pos.x + 1, pos.y}
+
+		diagonalDownLeft := Position{position.x - 1, position.y}
+		diagonalDownRight := Position{position.x + 1, position.y}
 
 		if isAir(diagonalDownLeft) {
-			pos = diagonalDownLeft
+			position = diagonalDownLeft
 			continue
 		} else if isAir(diagonalDownRight) {
-			pos = diagonalDownRight
+			position = diagonalDownRight
 			continue
 		}
 		break
 	}
-	setRuneAt('o', Position{pos.x, pos.y - 1})
-	return nil, Position{pos.x, pos.y - 1}
+	return Position{position.x, position.y - 1}, nil
 }
 
 func setMaxBounds(pathList [][]Position) {
@@ -124,8 +119,9 @@ func setMaxBounds(pathList [][]Position) {
 // cave
 
 func initCave(pathList [][]Position, hasFloor bool) {
-	columnsCount := 2*(maxY+4) - 1
+	columnsCount := 2*(maxY+1+xOffset) - 1
 	rowCount := maxY + 3
+
 	cave = make([][]rune, rowCount)
 	for y := 0; y < rowCount; y++ {
 		cave[y] = make([]rune, columnsCount)
@@ -138,32 +134,34 @@ func initCave(pathList [][]Position, hasFloor bool) {
 		}
 	}
 
-	for _, rockPath := range pathList {
-		setRockPaths(rockPath)
+	for _, path := range pathList {
+		setRocksAtPath(path)
 	}
 }
 
-func setRockPaths(path []Position) {
-	prev := path[0]
+func setRocksAtPath(path []Position) {
+	prevPos := path[0]
 	for i := 1; i < len(path); i++ {
-		curr := path[i]
-		line := getLine(prev, curr)
-		setRockLine(line)
-		prev = curr
+		currPos := path[i]
+		setRockForLine(createLineBetweenPoints(prevPos, currPos))
+		prevPos = currPos
 	}
 }
 
-func getLine(prev, curr Position) (line []Position) {
-	dx := getVerticalDistance(prev, curr)
-	dy := getHorizontalDistance(prev, curr)
-	for i := 0; i < int(math.Abs(float64(dx))); i++ {
-		line = append(line, Position{prev.x + sgn(dx)*i, curr.y})
+func createLineBetweenPoints(prevPos, currPos Position) (line []Position) {
+	dx := getVerticalDistance(prevPos, currPos)
+	dy := getHorizontalDistance(prevPos, currPos)
+	for i := 0; i < abs(dx); i++ {
+		line = append(line, Position{prevPos.x + sgn(dx)*i, currPos.y})
 	}
-	for i := 0; i < int(math.Abs(float64(dy))); i++ {
-		line = append(line, Position{curr.x, prev.y + sgn(dy)*i})
+	for i := 0; i < abs(dy); i++ {
+		line = append(line, Position{currPos.x, prevPos.y + sgn(dy)*i})
 	}
-	line = append(line, curr)
-	return line
+	return append(line, currPos)
+}
+
+func abs(number int) int {
+	return int(math.Abs(float64(number)))
 }
 
 func getVerticalDistance(pos1, pos2 Position) int {
@@ -184,39 +182,38 @@ func sgn(a int) int {
 	return 0
 }
 
-func setRockLine(path []Position) {
+func setRockForLine(path []Position) {
 	for _, pos := range path {
 		setRuneAt('#', pos)
 	}
 }
 
 func setRuneAt(r rune, pos Position) {
-	x := pos.x - sandPos.x + maxY + 3
+	x := pos.x - sandStartingPosition.x + maxY + xOffset
 	y := pos.y
 	cave[y][x] = r
 }
 
 func isAir(pos Position) bool {
-	x := pos.x - sandPos.x + maxY + 3
+	x := pos.x - sandStartingPosition.x + maxY + xOffset
 	y := pos.y
 	return cave[y][x] == '.' || cave[y][x] == '+'
 }
 
 // parsing
 
-func parse(lines []string) (pathList [][]Position) {
+func parsePathLines(lines []string) (pathList [][]Position) {
 	for _, line := range lines {
-		rockPath := parseLine(line)
-		pathList = append(pathList, rockPath)
+		pathList = append(pathList, parsePathLine(line))
 	}
 	return pathList
 }
 
-func parseLine(line string) (path []Position) {
-	coords := strings.Split(line, " -> ")
-	for _, c := range coords {
-		s := strings.Split(c, ",")
-		path = append(path, Position{util.GetInt(s[0]), util.GetInt(s[1])})
+func parsePathLine(line string) (path []Position) {
+	positions := strings.Split(line, " -> ")
+	for _, dimension := range positions {
+		splitted := strings.Split(dimension, ",")
+		path = append(path, Position{util.GetInt(splitted[0]), util.GetInt(splitted[1])})
 	}
 	return path
 }
