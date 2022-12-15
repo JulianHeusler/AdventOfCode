2619ec5e87ff789b64513ff70462c91e11dbf848
Julian Heusler <udxqd@kit.edu>
2022-12-15 19:30:49 +0100

    Solve day15 part 1

--- day15/day15.go
+++ day15/day15.go
@@ -18,7 +18,88 @@ type Sensor struct {
 
 func Solve(lines []string) (part1, part2 int) {
 	sensors := parse(lines)
-	return solvePart1(sensors, 10), 0
+	return chris(sensors, 2000000), 0
+}
+
+func chris(sensors []Sensor, lineNumber int) int {
+	minX, maxX := findBounds(sensors)
+	lenght := maxX - minX + 1
+	line := make([]bool, lenght)
+
+	for _, sensor := range sensors {
+		closestBeaconDistance := TaxicabDistance(sensor.position, sensor.closestBeacon)
+		distanceToLine := int(math.Abs(float64(sensor.position.Y - lineNumber)))
+
+		if distanceToLine <= closestBeaconDistance {
+			from, to := getInterval(sensor.position.X, closestBeaconDistance-distanceToLine)
+
+			if from < minX {
+				line = append(make([]bool, minX-from), line...)
+				minX = from
+			}
+
+			if maxX < to {
+				line = append(line, make([]bool, to-maxX)...)
+				maxX = to
+			}
+
+			for i := from - minX; i <= to-minX; i++ {
+				line[i] = true
+			}
+		}
+	}
+
+	return countAndPrint(line) - countAlreadyKnownBeacons(sensors, lineNumber)
+}
+
+func countAndPrint(line []bool) (count int) {
+	for _, b := range line {
+		if b {
+			count++
+			fmt.Print("#")
+		} else {
+			fmt.Print(".")
+		}
+	}
+	return count
+}
+
+func countAlreadyKnownBeacons(sensors []Sensor, y int) (count int) {
+	var temp []Position
+	for _, sensor := range sensors {
+		if sensor.closestBeacon.Y == y && !contains(temp, sensor.closestBeacon) {
+			temp = append(temp, sensor.closestBeacon)
+		}
+	}
+	return len(temp)
+}
+
+func contains(positions []Position, candidate Position) bool {
+	for _, position := range positions {
+		if position == candidate {
+			return true
+		}
+	}
+	return false
+}
+
+func getInterval(x, d int) (from, to int) {
+	return x - d, x + d
+}
+
+func findBounds(sensors []Sensor) (minX, maxX int) {
+	minX = math.MaxInt
+	maxX = math.MinInt
+	for _, sensor := range sensors {
+		currentX := sensor.position.X
+		if currentX < minX {
+			minX = currentX
+		}
+		if currentX > maxX {
+			maxX = currentX
+		}
+	}
+	return minX, maxX
 }
 
 func solvePart1(sensors []Sensor, lineNumber int) (part1 int) {
@@ -60,11 +141,11 @@ func TaxicabDistance(from Position, to Position) int {
 
 func parse(lines []string) (sensors []Sensor) {
 	for _, line := range lines {
-		sensorRegex := util.FindStringSubmatch(line, `Sensor at x=(-*\d+), y=(-*\d+):`)
+		sensorRegex := util.FindStringSubmatch(line, `Sensor at x=(-{0,1}\d+), y=(-{0,1}\d+):`)
 		sensorX := util.GetInt(sensorRegex[1])
 		sensorY := util.GetInt(sensorRegex[2])
 
-		beaconRegex := util.FindStringSubmatch(line, `closest beacon is at x=(-*\d+), y=(-*\d+)`)
+		beaconRegex := util.FindStringSubmatch(line, `closest beacon is at x=(-{0,1}\d+), y=(-{0,1}\d+)`)
 		beaconX := util.GetInt(beaconRegex[1])
 		beaconY := util.GetInt(beaconRegex[2])
 
--- day15/day15_test.go
+++ day15/day15_test.go
@@ -8,6 +8,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestSolve2(t *testing.T) {
+	lines := []string{
+		"Sensor at x=0, y=0: closest beacon is at x=0, y=-5",
+	}
+	resultPart1, resultPart2 := day15.Solve(lines)
+	assert.Equal(t, 26, resultPart1)
+	assert.Equal(t, 0, resultPart2)
+}
+
+
 func TestSolve(t *testing.T) {
 	resultPart1, resultPart2 := day15.Solve(reader.ReadExampleInput(15))
 	assert.Equal(t, 26, resultPart1)
@@ -19,10 +29,11 @@ func TestDistance(t *testing.T) {
 	assert.Equal(t, 12, day15.TaxicabDistance(day15.Position{6, 6}, day15.Position{0, 0}))
 	assert.Equal(t, 0, day15.TaxicabDistance(day15.Position{0, 0}, day15.Position{0, 0}))
 	assert.Equal(t, 1, day15.TaxicabDistance(day15.Position{0, 0}, day15.Position{0, 1}))
+	assert.Equal(t, 11, day15.TaxicabDistance(day15.Position{7, 9}, day15.Position{5, 18}))
 }
 
 func TestSolveRealInput(t *testing.T) {
 	resultPart1, resultPart2 := day15.Solve(reader.ReadInput(15))
-	assert.Equal(t, 0, resultPart1)
+	assert.Equal(t, 5256611, resultPart1)
 	assert.Equal(t, 0, resultPart2)
 }
