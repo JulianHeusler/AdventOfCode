330e873dcfba98141585252888db14d99eb638e3
Julian Heusler <julian.heusler@cas.de>
2022-12-15 16:32:29 +0100

    WIP day15

--- day15/day15_test.go
+++ day15/day15_test.go
@@ -14,6 +14,13 @@ func TestSolve(t *testing.T) {
 	assert.Equal(t, 0, resultPart2)
 }
 
+func TestDistance(t *testing.T) {
+	assert.Equal(t, 12, day15.TaxicabDistance(day15.Position{0, 0}, day15.Position{6, 6}))
+	assert.Equal(t, 12, day15.TaxicabDistance(day15.Position{6, 6}, day15.Position{0, 0}))
+	assert.Equal(t, 0, day15.TaxicabDistance(day15.Position{0, 0}, day15.Position{0, 0}))
+	assert.Equal(t, 1, day15.TaxicabDistance(day15.Position{0, 0}, day15.Position{0, 1}))
+}
+
 func TestSolveRealInput(t *testing.T) {
 	resultPart1, resultPart2 := day15.Solve(reader.ReadInput(15))
 	assert.Equal(t, 0, resultPart1)
