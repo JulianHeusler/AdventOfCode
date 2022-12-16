97285581496ad96f816dcd51d34c116ec92ac491
Julian Heusler <julian.heusler@cas.de>
2022-12-16 12:59:02 +0100

    WIP day13

--- adventofcode.go
+++ adventofcode.go
@@ -3,12 +3,12 @@ package main
 import (
 	"log"
 
-	"adventofcode/day15"
+	"adventofcode/day13"
 	"adventofcode/reader"
 )
 
 func main() {
-	resultPart1, resultPart2 := day15.Solve(reader.ReadInput(15))
+	resultPart1, resultPart2 := day13.Solve(reader.ReadInput(13))
 	log.Printf("Part 1: %v\n", resultPart1)
 	log.Printf("Part 2: %v\n", resultPart2)
 }
--- day13/day13.go
+++ day13/day13.go
@@ -2,6 +2,7 @@ package day13
 
 import (
 	"adventofcode/util"
+	"math"
 	"strings"
 )
 
@@ -10,9 +11,64 @@ type Packet struct {
 	sub    []*Packet
 }
 
+type Bracket struct {
+	value int
+	depth int
+}
+
 func Solve(lines []string) (part1, part2 int) {
-	parse(lines)
-	return 0, 0
+	packets := parse2(lines)
+	return solvePart1(packets), 0
+}
+
+func solvePart1(packets [][]Bracket) (part1 int) {
+	for i := 0; i < len(packets); i += 2 {
+		if rightOrder2(packets[i], packets[i+1]) {
+			part1 += i + 1
+		}
+	}
+	return part1
+}
+
+func rightOrder2(left, right []Bracket) bool {
+	x := int(math.Min(float64(len(left)), float64(len(right))))
+	for i := 0; i < x; i++ {
+		currL := left[i]
+		currR := right[i]
+		if left[i].depth == right[i].depth {
+			if left[i].value > right[i].value {
+				return false
+			}
+		} else {
+			if currL.depth < currR.depth {
+				currL.depth++
+				rightOrder2(append([]Bracket{currL}, left[i:]...), right[i:])
+			} else {
+				currR.depth++
+				rightOrder2(left[i:], append([]Bracket{currR}, right[i:]...))
+			}
+			break
+		}
+	}
+
+	return true
+}
+
+func rightOrder(left, right []Bracket) bool {
+	i := 0
+	if left[i].depth == right[i].depth {
+		if left[i].value > right[i].value {
+			return false
+		}
+	} else if left[i].depth > right[i].depth {
+		return false
+	}
+
+	if rightOrder(left[i+1:], right[i+1:]) {
+		return true
+	}
+
+	return true
 }
 
 var packets []Packet
@@ -22,6 +78,34 @@ func parse(lines []string) {
 		packets = append(packets, *parseLine(lines[i]))
 		packets = append(packets, *parseLine(lines[+1]))
 	}
+}
+
+func parse2(lines []string) (pakets [][]Bracket) {
+	for _, line := range lines {
+		if line != "" {
+			pakets = append(pakets, parseLine2(line))
+		}
+	}
+	return pakets
+}
+
+func parseLine2(line string) (packet []Bracket) {
+	currentDepth := 0
+	for i := 0; i < len(line)-1; i++ {
+		current := line[i]
+		next := line[i+1]
+		if current == '[' {
+			currentDepth++
+			if next == ']' {
+				packet = append(packet, Bracket{-1, currentDepth})
+			}
+		} else if current == ']' {
+			currentDepth--
+		} else if current != ',' {
+			packet = append(packet, Bracket{util.GetInt(string(current)), currentDepth})
+		}
+	}
+	return packet
 }
 
 func parseLine(line string) (p *Packet) {
