323aaeb23855511fbb4f5ae66d304cd6abf0d79e
Julian Heusler <julian.heusler@cas.de>
2022-12-19 12:51:47 +0100

    Add parsing day19

--- day19/day19.go
+++ day19/day19.go
@@ -1,5 +1,35 @@
 package day19
 
+import "adventofcode/util"
+
+type Blueprint struct {
+	robots []RobotRecipe
+}
+
+type RobotRecipe struct {
+	produces     string // maybe add enum?
+	oreCost      int
+	clayCost     int
+	obsidianCost int
+}
+
 func Solve(lines []string) (part1 int, part2 int) {
+	parse(lines)
 	return 0, 0
+}
+
+// keep in mind: blueprint index + 1
+func parse(lines []string) (blueprints []Blueprint) {
+	for _, line := range lines {
+		oreRegex := util.FindStringSubmatch(line, `Each ore robot costs (\d+) ore.`)
+		oreRobot := RobotRecipe{"ore", util.GetInt(oreRegex[1]), 0, 0}
+		clayRegex := util.FindStringSubmatch(line, `Each clay robot costs (\d+) ore.`)
+		clayRobot := RobotRecipe{"clay", util.GetInt(clayRegex[1]), 0, 0}
+		obsidianRegex := util.FindStringSubmatch(line, `Each obsidian robot costs (\d+) ore and (\d+) clay.`)
+		obsidianRobot := RobotRecipe{"obsidian", util.GetInt(obsidianRegex[1]), util.GetInt(obsidianRegex[2]), 0}
+		geodeRegex := util.FindStringSubmatch(line, `Each geode robot costs (\d+) ore and (\d+) obsidian.`)
+		geodeRobot := RobotRecipe{"geode", util.GetInt(geodeRegex[1]), 0, util.GetInt(geodeRegex[2])}
+		blueprints = append(blueprints, Blueprint{[]RobotRecipe{oreRobot, clayRobot, obsidianRobot, geodeRobot}})
+	}
+	return blueprints
 }
--- day19/day19_test.go
+++ day19/day19_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSolve2(t *testing.T) {
+func TestSolve(t *testing.T) {
 	lines := []string{
 		"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.",
 		"Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.",
