26e6971b20e0407f04883f6582619b94ac965167
Julian Heusler <julian.heusler@cas.de>
2022-12-19 12:25:13 +0100

    Init Day19

---
+++ day19/day19.go
@@ -0,0 +1,5 @@
+package day19
+
+func Solve(lines []string) (part1 int, part2 int) {
+	return 0, 0
+}
---
+++ day19/day19_test.go
@@ -0,0 +1,26 @@
+package day19_test
+
+import (
+	"adventofcode/day19"
+	"adventofcode/reader"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve2(t *testing.T) {
+	lines := []string{
+		"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.",
+		"Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.",
+	}
+
+	resultPart1, resultPart2 := day19.Solve(lines)
+	assert.Equal(t, 33, resultPart1)
+	assert.Equal(t, 0, resultPart2)
+}
+
+func TestSolveRealInput(t *testing.T) {
+	resultPart1, resultPart2 := day19.Solve(reader.ReadInput(19))
+	assert.Equal(t, 0, resultPart1)
+	assert.Equal(t, 0, resultPart2)
+}
