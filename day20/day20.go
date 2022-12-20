6731379d079e67f857019ecac6210729587e97a9
Julian Heusler <julian.heusler@cas.de>
2022-12-20 17:03:38 +0100

    WIP

--- day20/day20.go
+++ day20/day20.go
@@ -5,47 +5,51 @@ import (
 	"fmt"
 )
 
+type Code struct {
+	value      int
+	startIndex int
+}
+
 func Solve(lines []string) (part1 int, part2 int) {
 	file := parse(lines)
 	return solvePart1(file), 0
 }
 
-func solvePart1(file []int) int {
+func solvePart1(file []Code) int {
 	mixedFile := mixing(file)
 
 	l := len(mixedFile)
-	i := indexOf(0, mixedFile)[0]
+	i := indexOf(0, mixedFile)
 
-	a := mixedFile[(i+1000)%l]
-	b := mixedFile[(i+2000)%l]
-	c := mixedFile[(i+3000)%l]
+	a := mixedFile[(i+1000)%l].value
+	b := mixedFile[(i+2000)%l].value
+	c := mixedFile[(i+3000)%l].value
 	return a + b + c
 }
 
-func mixing(file []int) []int {
-	var fileCopy = make([]int, len(file))
+func mixing(file []Code) []Code {
+	var fileCopy = make([]Code, len(file))
 	copy(fileCopy, file)
 
-	for i, number := range file {
-		t := indexOf(number, fileCopy)
-		
-
-		newIndex := (i + number) % (len(file))
+	for _, code := range file {
+		newIndex := (indexOf(code.startIndex, fileCopy) + code.value) % (len(file))
 		if newIndex < 0 {
-			newIndex = len(file) + newIndex
+			newIndex = len(file) + newIndex - 1
+			if newIndex == 0 {
+				newIndex = len(fileCopy)
+			}
 		}
-		l := file[newIndex]
-		r := file[newIndex+1]
-		insertIndex := findIndexBetween(fileCopy, l, r)
+		insertIndex := newIndex
 
-		fileCopy = remove(fileCopy, t[0])
-		fileCopy = insert(fileCopy, insertIndex, number)
+		toRemoveIndex := indexOf(code.startIndex, fileCopy)
+		fileCopy = remove(fileCopy, toRemoveIndex)
+		fileCopy = insert(fileCopy, insertIndex, code)
 	}
 
 	return fileCopy
 }
 
-func findIndexBetween(file []int, leftNumber, rightNumber int) int {
+func findIndexBetween(file []Code, leftNumber, rightNumber Code) int {
 	for i := range file {
 		if file[i] == leftNumber && file[(i+1)%len(file)] == rightNumber {
 			return i
@@ -55,31 +59,7 @@ func findIndexBetween(file []int, leftNumber, rightNumber int) int {
 	return -1
 }
 
-func temp(file []int, number, oldIndex int) []int {
-	if number == 0 {
-		return file
-	}
-
-	var fileWithoutNumber = make([]int, len(file))
-	copy(fileWithoutNumber, file)
-
-	//oldIndex := indexOf(number, file)
-
-	fileWithoutNumber = remove(fileWithoutNumber, oldIndex)
-
-	newIndex := (oldIndex + number) % (len(fileWithoutNumber) + 1)
-
-	if newIndex < 0 {
-		newIndex = len(fileWithoutNumber) + newIndex
-	}
-
-	// fileWithNumber := append(fileWithoutNumber[:newIndex+1], fileWithoutNumber[newIndex:]...)
-	//	fileWithNumber[newIndex] = number
-	fileWithNumber := insert(fileWithoutNumber, newIndex, number)
-	return fileWithNumber
-}
-
-func remove(file []int, index int) []int {
+func remove(file []Code, index int) []Code {
 	if len(file) == index {
 		return file[:index]
 	}
@@ -87,7 +67,7 @@ func remove(file []int, index int) []int {
 }
 
 // 0 <= index <= len(file)
-func insert(file []int, index int, value int) []int {
+func insert(file []Code, index int, value Code) []Code {
 	if len(file) == index { // nil or empty slice or after last element
 		return append(file, value)
 	}
@@ -101,20 +81,21 @@ func swap(file []int, a, b int) []int {
 	return file
 }
 
-func parse(lines []string) (file []int) {
-	for _, line := range lines {
-		file = append(file, util.GetInt(line))
+func parse(lines []string) (file []Code) {
+	for i, line := range lines {
+		file = append(file, Code{util.GetInt(line), i})
 	}
 	return file
 }
 
-func indexOf(element int, file []int) (indexList []int) {
-	for index, number := range file {
-		if element == number {
-			indexList = append(indexList, index)
+func indexOf(startPos int, file []Code) int {
+	for index, code := range file {
+		if code.startIndex == startPos {
+			return index
 		}
 	}
-	return indexList
+	fmt.Println("not found")
+	return -1
 }
 
 func contains(i []int, x int) bool {
