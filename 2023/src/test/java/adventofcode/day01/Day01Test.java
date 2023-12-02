8eab560940e72b7d43ac3ffc7b2d708951ece5d5
Julian Heusler <udxqd@kit.edu>
2023-12-02 14:06:20 +0100

    Add test for part 1

---
+++ 2023/src/test/java/adventofcode/day01/Day01Test.java
@@ -0,0 +1,47 @@
+package adventofcode.day01;
+
+import adventofcode.util.Parser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day01Test {
+
+    private final Day01 day01 = new Day01();
+
+    @Test
+    void testInputPart1() {
+        String testInput = """
+                1abc2
+                pqr3stu8vwx
+                a1b2c3d4e5f
+                treb7uchet
+                    """;
+
+        assertEquals(142, day01.solvePart1(testInput));
+    }
+
+    @Test
+    void testRealInputPart1() {
+        assertEquals(55607, day01.solvePart1(Parser.readInputFile(1)));
+    }
+
+    @Test
+    void testInputPart2() {
+        assertEquals(281, day01.solvePart1(
+                """
+                        two1nine
+                        eightwothree
+                        abcone2threexyz
+                        xtwone3four
+                        4nineeightseven2
+                        zoneight234
+                        7pqrstsixteen
+                        """));
+    }
+
+    @Test
+    void testRealInputPart2() {
+        assertEquals(55607, day01.solvePart1(Parser.readInputFile(1)));
+    }
+}
