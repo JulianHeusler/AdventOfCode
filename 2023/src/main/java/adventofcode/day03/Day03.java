4d37b01f0b54a5aac35452fd77ce16d373e5950c
Julian Heusler <udxqd@kit.edu>
2023-12-03 15:16:40 +0100

    Solve part 2 day 3

--- 2023/src/main/java/adventofcode/day03/Day03.java
+++ 2023/src/main/java/adventofcode/day03/Day03.java
@@ -4,29 +4,75 @@ import adventofcode.util.AbstractDay;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day03 extends AbstractDay {
+    record PartNumber(int number, Position gearPosition) {
+    }
+
+    record Position(int y, int x) {
+    }
+
     @Override
     public int solvePart1(String input) {
-        System.out.println(foo(input));
-        return foo(input).stream().mapToInt(Integer::intValue).sum();
+        return getPartNumbers(input).stream().mapToInt(Integer::intValue).sum();
     }
 
     @Override
     public int solvePart2(String input) {
-        return 0;
+        List<Integer> gearRatios = getPartNumbersAdjacentToGear(input)
+                .stream()
+                .collect(Collectors.groupingBy(PartNumber::gearPosition))
+                .values()
+                .stream()
+                .filter(partNumbersPerGear -> partNumbersPerGear.size() == 2)
+                .map(partNumbersPerGear -> partNumbersPerGear
+                        .stream()
+                        .mapToInt(PartNumber::number)
+                        .reduce(1, Math::multiplyExact))
+                .toList();
+        return gearRatios.stream().mapToInt(Integer::intValue).sum();
     }
 
-    private List<Integer> foo(String input) {
+    private List<PartNumber> getPartNumbersAdjacentToGear(String input) {
+        List<PartNumber> partNumbers = new ArrayList<>();
+        StringBuilder currentNumber = new StringBuilder();
+        Position gearPosition = null;
+
+        String[] matrix = input.split("\n");
+        for (int y = 0; y < matrix.length; y++) {
+            for (int x = 0; x < matrix[y].length(); x++) {
+                char current = matrix[y].charAt(x);
+                if (Character.isDigit(current)) {
+                    currentNumber.append(current);
+                    Position position = isAdjacentToGear(matrix, y, x);
+                    if (position != null) {
+                        gearPosition = position;
+                    }
+                } else {
+                    if (gearPosition != null) {
+                        partNumbers.add(new PartNumber(Integer.parseInt(currentNumber.toString()), gearPosition));
+                        gearPosition = null;
+                    }
+                    currentNumber = new StringBuilder();
+                }
+            }
+            // at the end of each row
+            if (gearPosition != null) {
+                partNumbers.add(new PartNumber(Integer.parseInt(currentNumber.toString()), gearPosition));
+                gearPosition = null;
+            }
+            currentNumber = new StringBuilder();
+        }
+        return partNumbers;
+    }
+
+    private List<Integer> getPartNumbers(String input) {
         List<Integer> partNumbers = new ArrayList<>();
         StringBuilder currentNumber = new StringBuilder();
         boolean adjacentToSymbol = false;
 
         String[] matrix = input.split("\n");
-        for (String s : matrix) {
-            assert s.length() == matrix[0].length();
-        }
-
         for (int y = 0; y < matrix.length; y++) {
             for (int x = 0; x < matrix[y].length(); x++) {
                 char current = matrix[y].charAt(x);
@@ -43,15 +89,12 @@ public class Day03 extends AbstractDay {
                     currentNumber = new StringBuilder();
                 }
             }
+            // at the end of each row
             if (adjacentToSymbol) {
                 partNumbers.add(Integer.valueOf(currentNumber.toString()));
                 adjacentToSymbol = false;
             }
             currentNumber = new StringBuilder();
-        }
-
-        if (adjacentToSymbol) {
-            partNumbers.add(Integer.valueOf(currentNumber.toString()));
         }
         return partNumbers;
     }
@@ -75,6 +118,27 @@ public class Day03 extends AbstractDay {
             }
         }
         return false;
+    }
+
+    private Position isAdjacentToGear(String[] matrix, int y, int x) {
+        int[][] directions = {
+                {-1, -1}, {-1, 0}, {-1, 1},
+                {0, -1}, /*0, 0*/ {0, 1},
+                {1, -1}, {1, 0}, {1, 1}
+        };
+
+        for (int[] direction : directions) {
+            int newY = y + direction[0];
+            int newX = x + direction[1];
+
+            if (isInBounds(matrix, newY, newX)) {
+                char currentChar = matrix[newY].charAt(newX);
+                if (currentChar == '*') {
+                    return new Position(newY, newX);
+                }
+            }
+        }
+        return null;
     }
 
     private boolean isInBounds(String[] matrix, int y, int x) {
