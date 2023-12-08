b3c062f7043eef0cd3f8027ebbd922b5e066517d
Julian Heusler <julian.heusler@cas.de>
2023-12-08 16:34:14 +0100

    Solve part 1 day 7

--- 2023/src/main/java/adventofcode/day07/Day07.java
+++ 2023/src/main/java/adventofcode/day07/Day07.java
@@ -1,99 +1,208 @@
 package adventofcode.day07;
 
-import adventofcode.util.AbstractDay;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
+
+import adventofcode.util.AbstractDay;
 
 public class Day07 extends AbstractDay {
 
-    record Hand(List<Character> cards, int bid) {
+	enum Type {
+		FiveOfKind,
+		FourOfKind,
+		FullHouse,
+		ThreeOfKind,
+		TwoPair,
+		OnePair,
+		HighCard;
 
-        private int isHigher(Hand other) {
-            if (isFullHouse(cards) && isFullHouse(other.cards)) {
-                return isHandHigherSingle(other);
-            }
-            if (isFullHouse(cards) && (sameChars(other.cards) < 4)) {
-                return 1;
-            }
-            if ((sameChars(cards) < 4) && isFullHouse(other.cards)) {
-                return -1;
-            }
+		int getRank() {
+			return Type.values().length - this.ordinal();
+		}
+	}
 
-            if (sameChars(cards) > sameChars(other.cards)) {
-                return 1;
-            }
-            if (sameChars(cards) < sameChars(other.cards)) {
-                return -1;
-            }
-            return isHandHigherSingle(other);
-        }
+	record Hand(List<Character> cards, int bid) {
 
-        private int isHandHigherSingle(Hand other) {
-            for (int i = 0; i < cards.size(); i++) {
-                if (Objects.equals(cards.get(i), other.cards.get(i))) {
-                    continue;
-                }
-                return getCardValue(cards.get(i)).compareTo(getCardValue(other.cards.get(i)));
-            }
-            throw new IllegalStateException();
-        }
+		public int compareTo(Hand other) {
+			int compareTo = isHigher(other);
+			return compareTo;
+		}
 
-        private Integer getCardValue(Character c) {
-            return switch (c) {
-                case 'A' -> 14;
-                case 'K' -> 13;
-                case 'Q' -> 12;
-                case 'J' -> 11;
-                case 'T' -> 10;
-                default -> Integer.parseInt(String.valueOf(c));
-            };
-        }
+		private int isHigher(Hand other) {
+			Type typeSelf = resolveType(cards);
+			int rankSelf = typeSelf.getRank();
+			Type typeOther = resolveType(other.cards);
+			int rankOther = typeOther.getRank();
 
-        private boolean isFullHouse(List<Character> cards) {
-            Object[] sortedHand = cards.stream().sorted().toArray();
-            return (sortedHand[0] == sortedHand[1] && sortedHand[1] == sortedHand[2] && sortedHand[3] == sortedHand[4])
-                    || (sortedHand[0] == sortedHand[1] && sortedHand[2] == sortedHand[3] && sortedHand[3] == sortedHand[4]);
-        }
+			if (rankSelf == rankOther) {
+//				if (getHighestValue(cards) > getHighestValue(other.cards)) {
+//					return 1;
+//				}
+//				if (getHighestValue(cards) < getHighestValue(other.cards)) {
+//					return -1;
+//				}
+				return isHandHigherSingle(other);
+			}
+			return Integer.compare(rankSelf, rankOther);
+		}
 
-        private int sameChars(List<Character> cards) {
-            return cards.stream()
-                    .collect(Collectors.groupingBy(c -> c, Collectors.counting()))
-                    .values().stream()
-                    .mapToInt(Math::toIntExact)
-                    .max().orElse(0);
-        }
-    }
+		@Override
+		public String toString() {
+			return String.format("Cards: %s, Bid: %d, Type: %s", cards, bid, resolveType(cards));
+		}
 
-    @Override
-    public int solvePart1(String input) {
-        List<Hand> hands = parseHands(input);
+		private int extracted(Hand other) {
+			if (isFullHouse(cards) && isFullHouse(other.cards)) {
+				int compareTo = getHighestValue(cards).compareTo(getHighestValue(other.cards));
+				if (compareTo == 0) {
+					return isHandHigherSingle(other);
+				}
+				return compareTo;
+			}
+			if (isFullHouse(cards) && (sameChars(other.cards) < 4)) {
+				return 1;
+			}
+			if ((sameChars(cards) < 4) && isFullHouse(other.cards)) {
+				return -1;
+			}
 
-        List<Hand> sorted = hands.stream().sorted(Hand::isHigher).toList();
-        int result = 0;
-        for (int rank = 1; rank <= sorted.size(); rank++) {
-            result += rank * sorted.get(rank - 1).bid;
-        }
+			if (sameChars(cards) == sameChars(other.cards) && sameChars(cards) > 1) {
+				int compareTo = getHighestValue(cards).compareTo(getHighestValue(other.cards));
+				if (compareTo == 0) {
+					return isHandHigherSingle(other);
+				}
+				return compareTo;
+			}
 
-        return result;
-    }
-
-    @Override
-    public int solvePart2(String input) {
-        return 0;
-    }
+			if (sameChars(cards) > sameChars(other.cards)) {
+				return 1;
+			}
+			if (sameChars(cards) < sameChars(other.cards)) {
+				return -1;
+			}
+			return isHandHigherSingle(other);
+		}
 
 
-    private List<Hand> parseHands(String input) {
-        List<Hand> hands = new ArrayList<>();
-        for (String line : input.split("\n")) {
-            List<Character> cards = new ArrayList<>();
-            String[] split = line.split(" ");
-            split[0].chars().forEach(card -> cards.add((char) card));
-            hands.add(new Hand(cards, Integer.parseInt(split[1])));
-        }
-        return hands;
-    }
+		private Integer getHighestValue(List<Character> cards) {
+			List<Character> maxCombo = cards.stream()
+					.collect(Collectors.groupingBy(c -> c)).values().stream().max((o1, o2) ->
+							{
+								if (o1.size() == o2.size()) {
+									return compareSingleCards(o1.getFirst(), o2.getFirst());
+								}
+								return Integer.compare(o1.size(), o2.size());
+							}
+					).orElseThrow();
+			return getCardValue(maxCombo.getFirst());
+		}
+
+		private int isHandHigherSingle(Hand other) {
+			for (int i = 0; i < cards.size(); i++) {
+				Integer cardValue = getCardValue(cards.get(i));
+				Integer cardValueOther = getCardValue(other.cards.get(i));
+				if (cardValue.equals(cardValueOther)) {
+					continue;
+				}
+				return cardValue.compareTo(cardValueOther);
+			}
+			throw new IllegalStateException();
+		}
+
+		private int compareSingleCards(char a, char b) {
+			return getCardValue(a).compareTo(getCardValue(b));
+		}
+
+		private Integer getCardValue(Character c) {
+			return switch (c) {
+				case 'A' -> 14;
+				case 'K' -> 13;
+				case 'Q' -> 12;
+				case 'J' -> 11;
+				case 'T' -> 10;
+				default -> Integer.parseInt(String.valueOf(c));
+			};
+		}
+
+
+	}
+
+	private static Type resolveType(List<Character> cards) {
+		int sameChars = sameChars(cards);
+		if (sameChars == 5) {
+			return Type.FiveOfKind;
+		}
+		if (sameChars == 4) {
+			return Type.FourOfKind;
+		}
+		if (isFullHouse2(cards)) {
+			return Type.FullHouse;
+		}
+		if (sameChars == 3) {
+			return Type.ThreeOfKind;
+		}
+		if (isTwoPair(cards)) {
+			return Type.TwoPair;
+		}
+		if (sameChars == 2) {
+			return Type.OnePair;
+		}
+		return Type.HighCard;
+	}
+
+	private static boolean isTwoPair(List<Character> cards) {
+		return cards.stream().collect(Collectors.groupingBy(c -> c, Collectors.counting())).size() == 3;
+	}
+
+	private static boolean isFullHouse(List<Character> cards) {
+		Object[] sortedHand = cards.stream().sorted().toArray();
+		return (sortedHand[0] == sortedHand[1] && sortedHand[1] == sortedHand[2] && sortedHand[3] == sortedHand[4])
+				|| (sortedHand[0] == sortedHand[1] && sortedHand[2] == sortedHand[3] && sortedHand[3] == sortedHand[4]);
+	}
+
+	private static boolean isFullHouse2(List<Character> cards) {
+		return cards.stream().collect(Collectors.groupingBy(c -> c, Collectors.counting())).size() == 2;
+	}
+
+	private static int sameChars(List<Character> cards) {
+		return cards.stream()
+				.collect(Collectors.groupingBy(c -> c, Collectors.counting()))
+				.values().stream()
+				.mapToInt(Math::toIntExact)
+				.max().orElse(0);
+	}
+
+	@Override
+	public int solvePart1(String input) {
+		List<Hand> hands = parseHands(input);
+
+		List<Hand> sorted = hands.stream().sorted(Hand::compareTo).toList();
+		sorted.forEach(hand -> System.out.println(hand));
+		long result = 0;
+		for (int rank = 1; rank <= sorted.size(); rank++) {
+			result += (long) rank * sorted.get(rank - 1).bid;
+		}
+
+		System.out.println(result);
+
+		return (int) result;
+	}
+
+	@Override
+	public int solvePart2(String input) {
+		return 0;
+	}
+
+
+	private List<Hand> parseHands(String input) {
+		List<Hand> hands = new ArrayList<>();
+		for (String line : input.split("\n")) {
+			List<Character> cards = new ArrayList<>();
+			String[] split = line.split(" ");
+			split[0].chars().forEach(card -> cards.add((char) card));
+			hands.add(new Hand(cards, Integer.parseInt(split[1])));
+		}
+		return hands;
+	}
 }
--- 2023/src/test/java/adventofcode/day07/Day07Test.java
+++ 2023/src/test/java/adventofcode/day07/Day07Test.java
@@ -1,52 +1,79 @@
 package adventofcode.day07;
 
-import adventofcode.util.AbstractDay;
-import adventofcode.util.ParseUtil;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import adventofcode.util.AbstractDay;
+import adventofcode.util.ParseUtil;
+
 class Day07Test {
-    private final AbstractDay day = new Day07();
-    private static final int DAY_NUMBER = 7;
+	private final AbstractDay day = new Day07();
+	private static final int DAY_NUMBER = 7;
 
-    @Test
-    void testInputPart1() {
-        String testInput = """
-                32T3K 765
-                T55J5 684
-                KK677 28
-                KTJJT 220
-                QQQJA 483
-                """;
-        assertEquals(6440, day.solvePart1(testInput));
-    }
+	@Test
+	void testInputPart1() {
+		String testInput = """
+				32T3K 765
+				T55J5 684
+				KK677 28
+				KTJJT 220
+				QQQJA 483
+				""";
+		assertEquals(6440, day.solvePart1(testInput));
+	}
 
-    @Test
-    void testInputPart1dadw() {
-        String testInput = """
-                22234 2
-                22233 3
-                """;
-        assertEquals(8, day.solvePart1(testInput));
-    }
+	@Test
+	void testInputPart1dadw() {
+		String testInput = """
+				22234 2
+				22233 3
+				""";
+		assertEquals(8, day.solvePart1(testInput));
+	}
 
-    @Test
-    void testRealInputPart1() {
-        assertEquals(251055711, day.solvePart1(ParseUtil.readInputFile(DAY_NUMBER)));
-    }
+	@Test
+	void testInputExample1() {
+		String testInput = """
+				2345A 1
+				Q2KJJ 13
+				Q2Q2Q 19
+				T3T3J 17
+				T3Q33 11
+				2345J 3
+				J345A 2
+				32T3K 5
+				T55J5 29
+				KK677 7
+				KTJJT 34
+				QQQJA 31
+				JJJJJ 37
+				JAAAA 43
+				AAAAJ 59
+				AAAAA 61
+				2AAAA 23
+				2JJJJ 53
+				JJJJ2 41
+				""";
+		assertEquals(6592, day.solvePart1(testInput));
+	}
 
-    @Test
-    void testInputPart2() {
-        String testInput = """
-                Time:      7  15   30
-                Distance:  9  40  200
-                """;
-        assertEquals(71503, day.solvePart2(testInput));
-    }
+	@Test
+	void testRealInputPart1() {
+		assertEquals(252295678, day.solvePart1(ParseUtil.readInputFile(DAY_NUMBER)));
+	}
 
-    @Test
-    void testRealInputPart2() {
-        assertEquals(24655068, day.solvePart2(ParseUtil.readInputFile(DAY_NUMBER)));
-    }
+	@Test
+	void testInputPart2() {
+		String testInput = """
+				Time:      7  15   30
+				Distance:  9  40  200
+				""";
+		assertEquals(71503, day.solvePart2(testInput));
+	}
+
+	@Test
+	void testRealInputPart2() {
+		assertEquals(24655068, day.solvePart2(ParseUtil.readInputFile(DAY_NUMBER)));
+	}
 }
