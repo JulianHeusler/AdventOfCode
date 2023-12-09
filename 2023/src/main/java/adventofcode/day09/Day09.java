4092309c679f3c844358a0b270032775bf2fc838
Julian Heusler <udxqd@kit.edu>
2023-12-09 13:35:17 +0100

    Refactor day 9 to use streams

--- 2023/src/main/java/adventofcode/day09/Day09.java
+++ 2023/src/main/java/adventofcode/day09/Day09.java
@@ -4,6 +4,7 @@ import adventofcode.util.AbstractDay;
 import adventofcode.util.ParseUtil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day09 extends AbstractDay {
@@ -11,33 +12,33 @@ public class Day09 extends AbstractDay {
 
     @Override
     public int solvePart1(String input) {
-        List<List<Integer>> histories = parseHistories(input);
-        long result = 0;
-        for (List<Integer> history : histories) {
-            result += solve(history);
-        }
-        return Math.toIntExact(result);
+        return parseHistories(input).stream()
+                .map(this::predictNextValue)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
-    private int solve(List<Integer> history) {
+    @Override
+    public int solvePart2(String input) {
+        return 0;
+    }
+
+    private int predictNextValue(List<Integer> history) {
         List<List<Integer>> sequences = getSequences(history);
         sequences.forEach(System.out::println);
-
-        int a = 0;
-        for (List<Integer> sequence : sequences.reversed()) {
-            a += sequence.getLast();
-        }
-        return a;
+        return sequences.reversed()
+                .stream()
+                .map(List::getLast)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
-
 
     private List<List<Integer>> getSequences(List<Integer> history) {
         List<List<Integer>> sequences = new ArrayList<>();
-        sequences.add(history);
         List<Integer> nextSequence = history;
         while (!isEmpty(nextSequence)) {
-            nextSequence = getSequence(nextSequence);
             sequences.add(nextSequence);
+            nextSequence = getSequence(nextSequence);
         }
         return sequences;
     }
@@ -46,29 +47,20 @@ public class Day09 extends AbstractDay {
         return nextSequence.stream().allMatch(i -> i == 0);
     }
 
-    @Override
-    public int solvePart2(String input) {
-        return 0;
-    }
-
     private List<Integer> getSequence(List<Integer> line) {
         List<Integer> newSequence = new ArrayList<>();
         if (line.size() == 1) {
             return List.of(0);
         }
-
         for (int i = 0; i < line.size() - 1; i++) {
-            int x = line.get(i + 1) - line.get(i);
-            newSequence.add(x);
+            newSequence.add(line.get(i + 1) - line.get(i));
         }
         return newSequence;
     }
 
     private List<List<Integer>> parseHistories(String input) {
-        List<List<Integer>> histories = new ArrayList<>();
-        for (String line : input.split("\n")) {
-            histories.add(ParseUtil.parseIntegerNumbers(line));
-        }
-        return histories;
+        return Arrays.stream(input.split("\n"))
+                .map(ParseUtil::parseIntegerNumbers)
+                .toList();
     }
 }
