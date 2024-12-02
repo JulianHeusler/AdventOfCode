e8681d9900f45831b05943d7c6f7780910185e85
Julian Heusler <julian.heusler@cas.de>
2024-12-02 16:21:04 +0100

    Init AoC 2024

---
+++ 2024/README.md
@@ -0,0 +1,3 @@
+# My solutions for [advent of code 2024](https://adventofcode.com/2024)
+
+Practicing Java this year with the focus on the streaming api
---
+++ 2024/src/main/java/adventofcode/util/AbstractDay.java
@@ -0,0 +1,8 @@
+package adventofcode.util;
+
+public abstract class AbstractDay {
+
+    public abstract long solvePart1(String input);
+
+    public abstract long solvePart2(String input);
+}
