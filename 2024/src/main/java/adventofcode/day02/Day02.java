adf8ab16745f0e52aa1cdcd86c147ba72a112d82
Julian Heusler <julian.heusler@cas.de>
2024-12-04 10:12:46 +0100

    Solve part 2 day 2

--- 2024/src/main/java/adventofcode/day02/Day02.java
+++ 2024/src/main/java/adventofcode/day02/Day02.java
@@ -7,6 +7,7 @@ import adventofcode.util.AbstractDay;
 import adventofcode.util.ParseUtil;
 
 public class Day02 extends AbstractDay {
+
 	@Override
 	public long solvePart1(String input) {
 		return parseInput(input).stream().filter(this::isSafe).count();
@@ -14,9 +15,33 @@ public class Day02 extends AbstractDay {
 
 	@Override
 	public long solvePart2(String input) {
-		return 0;
+		return parseInput(input).stream().filter(report -> isSafe(report.levels, false)).count();
 	}
 
+	private boolean isSafe(List<Integer> levels, boolean skippedOne) {
+		List<Integer> deltas = new ArrayList<>();
+		for (int i = 0; i < levels.size() - 1; i++) {
+			int delta = levels.get(i) - levels.get(i + 1);
+			deltas.add(delta);
+			if (isInvalidDelta(delta) || haveDifferentSigns(deltas)) {
+				if (skippedOne) {
+					return false;
+				}
+				return isSafe(removeLevel(levels, i - 1), true)
+						|| isSafe(removeLevel(levels, i), true)
+						|| isSafe(removeLevel(levels, i + 1), true);
+			}
+		}
+		return true;
+	}
+
+
+	private List<Integer> removeLevel(List<Integer> list, int index) {
+		ArrayList<Integer> copy = new ArrayList<>();
+		copy.addAll(list.subList(0, Math.max(0, index)));
+		copy.addAll(list.subList(Math.min(index + 1, list.size()), list.size()));
+		return copy;
+	}
 
 	private boolean isSafe(Report report) {
 		List<Integer> deltas = new ArrayList<>();
@@ -24,13 +49,19 @@ public class Day02 extends AbstractDay {
 			deltas.add(report.levels.get(i) - report.levels.get(i + 1));
 		}
 
-		if (!allPositive(deltas) && !allNegative(deltas)) {
+		if (haveDifferentSigns(deltas)) {
 			return false;
 		}
-		if (deltas.stream().map(Math::abs).anyMatch(integer -> integer < 1 || 3 < integer)) {
-			return false;
-		}
-		return true;
+		return deltas.stream().noneMatch(Day02::isInvalidDelta);
+	}
+
+	private static boolean isInvalidDelta(Integer integer) {
+		int abs = Math.abs(integer);
+		return 1 > abs || abs > 3;
+	}
+
+	private static boolean haveDifferentSigns(List<Integer> deltas) {
+		return !allPositive(deltas) && !allNegative(deltas);
 	}
 
 	private static boolean allPositive(List<Integer> deltas) {
--- 2024/src/test/java/adventofcode/day02/Day02Test.java
+++ 2024/src/test/java/adventofcode/day02/Day02Test.java
@@ -40,11 +40,29 @@ class Day02Test {
 				1 3 6 7 9
 				""";
 
-		assertEquals(0, day02.solvePart2(testInput));
+		assertEquals(4, day02.solvePart2(testInput));
+	}
+
+	@Test
+	void testInputPart2extra() {
+		String testInput = """
+				48 46 47 49 51 54 56
+				1 1 2 3 4 5
+				1 2 3 4 5 5
+				5 1 2 3 4 5
+				1 4 3 2 1
+				1 6 7 8 9
+				1 2 3 4 3
+				9 8 7 6 7
+				7 10 8 10 11
+				29 28 27 25 26 25 22 20
+				""";
+
+		assertEquals(10, day02.solvePart2(testInput));
 	}
 
 	@Test
 	void testRealInputPart2() {
-		assertEquals(0, day02.solvePart2(ParseUtil.readInputFile(2)));
+		assertEquals(324, day02.solvePart2(ParseUtil.readInputFile(2)));
 	}
 }
